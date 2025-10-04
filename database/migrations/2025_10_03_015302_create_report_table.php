751e089f30bce4ca64f1fc781b283c03c960d572
Muhamad Gia Mardhotillah <gia95194@gmail.com>
2025-10-04 17:20:09 +0700

    modified report migration

--- database/migrations/2025_10_03_015302_create_report_table.php
+++ database/migrations/2025_10_03_015302_create_report_table.php
@@ -15,7 +15,13 @@ return new class extends Migration
             $table->bigIncrements('rpt_id');
             $table->unsignedBigInteger('rpt_usr_id');
             $table->date('rpt_date');
+            $table->tinyInteger('rpt_month'); // 1-12
+            $table->year('rpt_year');
+            $table->unsignedBigInteger('rpt_created_by')->nullable();
+            $table->unsignedBigInteger('rpt_updated_by')->nullable();
+            $table->unsignedBigInteger('rpt_deleted_by')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
